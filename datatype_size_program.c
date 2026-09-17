dd1a45103baa9f38cbcaad25cf62eb0920c507da
Prashant Murthy <prashant.m.vvl@gmail.com>
2026-09-17 23:40:31 +0530

    size of datatypes

---
+++ datatype_size_program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<stdbool.h>
+int main()
+{
+    printf("the size of void is %d\n", sizeof(void));
+    printf("the size of int is %d\n", sizeof(int));
+    printf("the size of char is %d\n", sizeof(char));
+    printf("the size of long int is %d\n", sizeof(long int));
+    printf("the size of float is %d\n", sizeof(float));
+    printf("the size of double is %d\n", sizeof(double));
+    printf("the size of bool is %d\n", sizeof(bool));
+     printf("the size of short int is %d\n", sizeof(short int));
+    return 0;
+}
