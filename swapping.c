bc0549900b68475c7794f4636f4770a162429fd1
Prashant Murthy <prashant.m.vvl@gmail.com>
2026-09-17 23:51:44 +0530

    using multiplication division

--- swapping.c
+++ swapping.c
@@ -6,8 +6,8 @@ void main()
     printf("enter the values of numbers");
     scanf("%d%d", &num1, &num2);
     printf("the values of the numbers before swapping is in: %d%d\n", num1, num2);
-    num1 = num1 + num2;
-    num2 = num1 - num2;
-    num1 = num1 - num2;
+    num1 = num1 * num2;
+    num2 = num1 / num2;
+    num1 = num1 / num2;
     printf("the values of the numbers swapped in: %d%d\n", num1, num2);
 }
