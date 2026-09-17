c30ac3b83f9a8f070d1e283b38aeb014db84f302
Prashant Murthy <prashant.m.vvl@gmail.com>
2026-09-17 23:49:35 +0530

    program to swap two numbers

---
+++ swapping.c
@@ -0,0 +1,13 @@
+// generate code to swap two numbers without using a third variable
+#include <stdio.h>
+void main()
+{
+    int num1, num2;
+    printf("enter the values of numbers");
+    scanf("%d%d", &num1, &num2);
+    printf("the values of the numbers before swapping is in: %d%d\n", num1, num2);
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+    printf("the values of the numbers swapped in: %d%d\n", num1, num2);
+}
